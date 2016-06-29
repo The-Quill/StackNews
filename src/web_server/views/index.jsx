95d2edaea64c079321335048f8e7477a4de564f7
Quill <seriouslyquill@gmail.com>
2016-06-29 20:54:31 +1000

    added css module

--- src/web_server/views/index.jsx
+++ src/web_server/views/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Head, Header, List, Footer } from '../components.es6'
+import styles from '../css_modules/body'
 
 var Index = React.createClass({
     render(){
         return <html>
             <Head></Head>
-            <body>
+            <body className={styles.body}>
                 <Header></Header>
                 <List items={this.props.items}></List>
                 <Footer></Footer>
