9753254aab110b6237aa44a816f6c0e17a88e206
Quill <seriouslyquill@gmail.com>
2016-06-30 23:34:44 +1000

    renamed the file

---
+++ src/web_server/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import path from 'path'
+
+import IndexRoute from './routes/index'
+import CssRoute from './routes/css'
+import CssMapRoute from './routes/cssmap'
+import ImageRoute from './routes/image'
+
+const app = express()
+let port = 4444
+
+app.get('/', IndexRoute);
+app.get('/[a-zA-Z]+.png', ImageRoute);
+app.get('/[a-zA-Z]+.css', CssRoute);
+app.get('/[a-zA-Z]+.css.map', CssMapRoute);
+
+//Route not found -- Set 404
+app.get('*', function(req, res) {
+    const { path } = req
+    console.log(` - Attempted to find variable at path: ${path}`)
+    res.json({
+        'route': 'Sorry this page does not exist!'
+    });
+});
+
+app.listen(port);
+console.log('Server is up and running at port: ' + port);
