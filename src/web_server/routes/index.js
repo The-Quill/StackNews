d0319c392a1fbb65315e431441b87f44669a7ebb
Quill <seriouslyquill@gmail.com>
2016-07-01 01:14:59 +1000

    Fixed to use Redis data instead of sample data

--- src/web_server/routes/index.js
+++ src/web_server/routes/index.js
@@ -2,41 +2,30 @@ import { RedisSession } from '../../modules/redis'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { Application } from '../components'
-// async function getPosts(){
-//     return await getPostsOrSomething();
-// }
+
+const session = new RedisSession();
+
+async function getPosts(offsetMultiplier, perPage){
+    // count
+    // zrangebyscore posts -inf +inf LIMIT 50000 10
+    let count = await session.client.zcardAsync('posts')
+    let offset = (offsetMultiplier === 0 ? 1 : offsetMultiplier) * perPage
+    let fetch = count - offset
+    return session.client.zrangebyscoreAsync(['posts', '-inf', '+inf', 'LIMIT', `${fetch}`, `${perPage}`])
+}
 let IndexRoute = function(req, res){
-    //let items = await getPosts();
-    let items = [
-        {
-            'owner:image': 'a.png',
-            'title': 'Test'
-        },
-        {
-            'owner:image': 'a.png',
-            'title': 'Test'
-        },
-        {
-            'owner:image': 'a.png',
-            'title': 'Test'
-        },
-        {
-            'owner:image': 'a.png',
-            'title': 'Test'
-        },
-        {
-            'owner:image': 'a.png',
-            'title': 'Test'
-        },
-        {
-            'owner:image': 'a.png',
-            'title': 'Test'
-        }
-    ]
     // const { path } = req
-    const app = <Application items={items} />
-    res.write(renderToString(app))
-    res.end()
+
+    getPosts(1, 30).then(async function(itemKeys) {
+        var posts = []
+        for (var i = 0; i < itemKeys.length; i++){
+            let post = await session.client.hgetallAsync(`post:${itemKeys[i]}`)
+            posts.push(post)
+        }
+        let app = <Application items={posts.reverse()} />
+        res.write(renderToString(app))
+        res.end()
+    })
 };
 
 export { IndexRoute as default }
