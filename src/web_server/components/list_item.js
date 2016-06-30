2bab7b310f86c1843b13d0cd314c0ae9f81fec46
Quill <seriouslyquill@gmail.com>
2016-07-01 02:46:17 +1000

    added post info

--- src/web_server/components/list_item.js
+++ src/web_server/components/list_item.js
@@ -10,9 +10,22 @@ const ListItem = React.createClass({
     },
     render () {
         return (
-            <div className="list-item">
-                <img src={this.props.item['owner:image']} />
-                <div className="content">{entities.decode(this.props.item.title)}</div>
+            <div className={`list-item ${this.props.item['owner:ismoderator'] ? 'moderatorPost' : ''}`}>
+                <div>
+                    <span className="score">
+                        {this.props.item.score || 0}
+                    </span>
+                    <img className="site-icon" src={this.props.item.site.icon_url} />
+                    <a target="_blank" href={
+                        `${this.props.item.site['site_url']}/q/${this.props.item.question_id}`
+                    } className="content">
+                        {entities.decode(this.props.item.title)}
+                    </a>
+                </div>
+                <div>
+                    <span>  posted by {this.props.item['owner:name']}</span>
+                    <img className="user-icon" src={this.props.item['owner:image']} />
+                </div>
             </div>
         )
     }
