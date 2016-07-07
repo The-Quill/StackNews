5d2130c9bdb583c16beaa8b5ba672f37207e7c5a
Quill <seriouslyquill@gmail.com>
2016-07-07 23:06:28 +1000

    fixed reducer

--- src/web/reducers/posts.js
+++ src/web/reducers/posts.js
@@ -1,14 +1,12 @@
 const PostsReducer = (state = [], action) => {
     switch (action.type){
         case "ADD_POST":
-            var data = Object.values();
             return [
                 ...state,
-                ...data
+                action.data
             ]
             break;
         case "REMOVE_POST":
-            var data = Object.values();
             return [
                 ...state.slice(0, action.index),
                 ...state.slice(action.index + 1)
