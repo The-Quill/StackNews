721e7dc132bd473c97905155504ddfefd679dca0
Quill <seriouslyquill@gmail.com>
2016-07-07 23:06:40 +1000

    Fixed application to use new index route

--- src/web/components/application.js
+++ src/web/components/application.js
@@ -3,14 +3,11 @@ import { Head, Header, List, Footer } from '../components'
 
 class Application extends React.Component {
     render(){
-        return <html>
-            <Head></Head>
-            <body className={'main'}>
-                <Header></Header>
-                <List items={this.props.items || []}></List>
-                <Footer></Footer>
-            </body>
-        </html>
+        return <div>
+            <Header></Header>
+            <List items={this.props.items || []}></List>
+            <Footer></Footer>
+        </div>
     }
 }
 
