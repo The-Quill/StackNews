86b222f4d51eba68818a04b904605306bee0fa19
Quill <seriouslyquill@gmail.com>
2016-07-07 23:06:00 +1000

    Removed image from header

--- src/web/components/header.js
+++ src/web/components/header.js
@@ -4,10 +4,9 @@ import Defaults from '../defaults'
 class Header extends React.Component {
     render(){
         return <nav>
-            <img src={Defaults.icon_url} />
             <h1>{Defaults.website_name}</h1>
         </nav>
     }
 }
 
-export default Header  
+export default Header
