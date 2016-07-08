69ea0439a43688f80a4e65bc70572fcd9cf62db2
Quill <seriouslyquill@gmail.com>
2016-07-09 09:52:13 +1000

    Added browser code

---
+++ src/web/browser.js
@@ -0,0 +1,54 @@
+import PostsReducer from './reducers/posts'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import { Application } from './components'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import 'whatwg-fetch';
+window.React = React
+
+var currentPage = 1;
+var store = createStore(PostsReducer)
+ReactDOM.render(
+    <Provider store={store}>
+        <Application />
+    </Provider>,
+    document.getElementById('root')
+)
+store.subscribe(() => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <Application />
+        </Provider>,
+        document.getElementById('root')
+    )
+})
+function addPosts(data){
+    let posts = data.posts;
+    console.log(posts)
+    posts.forEach(post => store.dispatch(
+        {
+            type: "ADD_POST",
+            data: post
+        }
+    ))
+}
+function getNew(){
+    fetch(`/data/${currentPage++}`)
+    .then(response => response.json().then(addPosts))
+}
+window.addEventListener('scroll', function(event)
+{
+    console.log(`scroll`)
+    var element = event.target;
+    if (element.scrollHeight == null){
+        element = element.scrollingElement;
+    }
+    let percentScrolled = (element.scrollTop / element.clientHeight) * 100;
+    if (percentScrolled > 70)
+    {
+        console.log('scrolled');
+        getNew()
+    }
+});
+getNew()
