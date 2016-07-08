bd2392ebcda349888f60d4c74bd7a5be7eabc6d8
Quill <seriouslyquill@gmail.com>
2016-07-09 09:50:06 +1000

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: ['whatwg-fetch', 'web/browser.js'],
+    resolve: {
+        modulesDirectories: ['node_modules', 'src'],
+        extension: ['', '.js', '.jsx', '.json', '.scss']
+    },
+    output: {
+        path: 'dist/web/resources',
+        filename: 'render.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                // query: {
+                //     presets: ['es2015', 'react']
+                // }
+            },
+            {
+                test: /.json$/,
+                loader: 'json-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
