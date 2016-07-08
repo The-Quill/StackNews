76ba1508c7c23f26f974e56aab185d7cad65a97d
Quill <seriouslyquill@gmail.com>
2016-07-09 09:52:07 +1000

    Rewrote index route

--- src/web/routes/index.js
+++ src/web/routes/index.js
@@ -1,46 +1,22 @@
-import { RedisSession } from '../../modules/redis'
-import { FetchPosts } from '../../modules/se_api'
 import React from 'react'
+import Route from '../../modules/route'
 import { renderToString } from 'react-dom/server'
 import { Application, Head } from '../components'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import PostsReducer from '../reducers/posts'
 
-const session = new RedisSession();
-
-
-let IndexRoute = function(req, res){
-    console.log(`Got route`)
-    // const { path } = req
+let IndexRoute = new Route('Index', true)
+IndexRoute.router = function(req, res){
     const store = createStore(PostsReducer)
-
-    // Render the component to a string
-
-
-    // Grab the initial state from our Redux store
     const preloadedState = store.getState()
-
-    FetchPosts(1, 30).then(async function(itemKeys) {
-        var posts = []
-        for (var i = 0; i < itemKeys.length; i++){
-            let post = await session.client.hgetallAsync(`post:${itemKeys[i]}`)
-            post.site = await session.client.hgetallAsync(`site:${post.site}`)
-            store.dispatch(
-                {
-                    type: "ADD_POST",
-                    data: post
-                }
-            )
-        }
-        const html = renderToString(
-            <Provider store={store}>
-                <Application items={store.getState().reverse()} />
-            </Provider>
-        )
-        res.write(renderFullPage(html, preloadedState))
-        res.end()
-    })
+    const html = renderToString(
+        <Provider store={store}>
+            <Application />
+        </Provider>
+    )
+    res.write(renderFullPage(html, preloadedState))
+    res.end()
 };
 function renderFullPage(html, preloadedState) {
     return `
@@ -52,10 +28,10 @@ function renderFullPage(html, preloadedState) {
                 <script>
                     window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
                 </script>
-                <script src="/static/bundle.js"></script>
+                <script src="/render.js"></script>
             </body>
         </html>
         `
 }
 
-export { IndexRoute as default }
+export default IndexRoute.router
