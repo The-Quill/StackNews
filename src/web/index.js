0e4dd6e5560060593ec6226d1fd267a5be4a8bec
Quill <seriouslyquill@gmail.com>
2016-07-09 09:51:33 +1000

    Removed redundant routes, added blocked and allowed file extension routes

--- src/web/index.js
+++ src/web/index.js
@@ -1,29 +1,30 @@
 import express from 'express'
 import path from 'path'
 
+import DataRoute from './routes/data'
+import FileRoute from './routes/file'
 import IndexRoute from './routes/index'
-import CssRoute from './routes/css'
-import CssMapRoute from './routes/cssmap'
-import ImageRoute from './routes/image'
-import TtfRoute from './routes/ttf'
+import BlockRoute from './routes/block'
+import FourOhFourRoute from './routes/404'
+
+import Defaults from './defaults'
 
 const app = express()
 let port = 4444
 
 app.get('/', IndexRoute);
-app.get('/[a-zA-Z]+.png', ImageRoute);
-app.get('/[a-zA-Z]+.css', CssRoute);
-app.get('/[a-zA-Z]+.css.map', CssMapRoute);
-app.get('/[a-zA-Z]+.ttf', TtfRoute);
+app.get('/data/:page', DataRoute);
 
-//Route not found -- Set 404
-app.get('*', function(req, res) {
-    const { path } = req
-    console.log(` - Attempted to find variable at path: ${path}`)
-    res.json({
-        'route': 'Sorry this page does not exist!'
-    });
-});
+app.get('/render.js', FileRoute);
 
+Defaults.allowedFileExtensions.forEach(
+    extension => app.get(`/[a-zA-Z]+.${extension}`, FileRoute)
+)
+Defaults.bannedFileExtensions.forEach(
+    extension => app.get(`/[a-zA-Z]+.${extension}`, BlockRoute)
+)
+
+app.get('*', FourOhFourRoute);
 app.listen(port);
+
 console.log('Server is up and running at port: ' + port);
