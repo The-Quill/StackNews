2ee1f5c68369367ec5a992324d78971d508f8f7b
Quill <seriouslyquill@gmail.com>
2016-07-09 09:50:18 +1000

    removed unnecessary routes

--- src/web/routes/css.js
+++
@@ -1,5 +0,0 @@
-let CssRoute = function(req, res){
-    res.sendFile('out.css', {root: './dist/web_server/scss'});
-};
-
-export { CssRoute as default }
--- src/web/routes/ttf.js
+++
@@ -1,7 +0,0 @@
-let TtfRoute = function(req, res){
-    const { path } = req
-    let fileName = path.substring(path.lastIndexOf('/') + 1, path.length)
-    res.sendFile(fileName, {root: './dist/web_server/resources'});
-};
-
-export { TtfRoute as default }
