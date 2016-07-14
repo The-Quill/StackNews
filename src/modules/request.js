da612d313df99e9e23d9c4c35d95ec85b3b2c3f7
Quill <seriouslyquill@gmail.com>
2016-07-14 16:53:19 +1000

    use more stable fetching model

--- src/modules/request.js
+++ src/modules/request.js
@@ -1,27 +1,28 @@
 import request from 'request-promise'
 import dom from 'cheerio'
 import errors from 'request-promise/errors'
+import sleep from 'sleep'
 
 async function Request(options){
     let defaultOptions = {
         gzip: true
     }
-    return new Promise(
-        (resolve, reject) => {
-            //console.log(`Fetching data from ${options.url}`)
-            request(Object.assign(defaultOptions, options))
-            .then(data => resolve(data))
-            .catch(errors.StatusCodeError, function (reason) {
-                console.log(`oh shit, the server responded with a ${reason.statusCode}. you messed up dude.`)
-            })
-            .catch(errors.RequestError, function (reason) {
-                if (reason.cause === "Error: socket hang up" || reason.cause === "Error: read ECONNRESET"){
-                    resolve(Request(options))
-                }
-                reject(`Shoddy options provided. URL was ${options.url} and reason was ${reason.cause}`)
-            });
+    var isInProgress = true;
+    var attempts = 0;
+    var result;
+    while(isInProgress){
+        attempts++;
+        try {
+            result = await request(Object.assign(defaultOptions, options))
+            isInProgress = false
+        } catch(error) {
+            await sleep.sleep(60)
+            if (attempts > 10){
+                return Promise.reject(error)
+            }
         }
-    );
+    }
+    return Promise.resolve(result)
 }
 async function HtmlRequest(options){
     let defaultOptions = {
