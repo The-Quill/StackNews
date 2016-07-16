ac45504de96a6b9630bc2c53a1d4e7f28e940aea
Quill <seriouslyquill@gmail.com>
2016-07-17 06:03:52 +1000

    caaaaaaaaaatch me when I faaaaaaaallll

--- src/web/routes/data.js
+++ src/web/routes/data.js
@@ -6,7 +6,12 @@ var DataRoute = new Route('Data', true);
 DataRoute.router = async function(req, res) {
     let { page } = req.params;
     let count = 30;
-    let posts = await LoadNewPosts(page, count);
+    try {
+        let posts = await LoadNewPosts(page, count);
+    } catch(error){
+        throw new Error(error);
+    }
+
     res.json({ posts: posts });
 }
 export default DataRoute.router
