a13b9d979d29f33f4e16e32178f9070c6b1fd9f7
Quill <seriouslyquill@gmail.com>
2016-07-17 03:19:20 +1000

    NO FILE FOR YOU

--- src/web/routes/file.js
+++ src/web/routes/file.js
@@ -3,6 +3,10 @@ import Route from '../../modules/route'
 var FileRoute = new Route('File', true);
 FileRoute.router = function(req, res) {
     const { path } = req
+    if (path == ""){
+        res.write('')
+        res.end()
+    }
     let file = path.replace(path.substring(0, path.lastIndexOf('/') + 1), '')
     res.sendFile(file, {root: './dist/web/resources'});
 };
