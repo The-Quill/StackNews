436952dca89425acade30148739ab2f3e4f218b2
Quill <seriouslyquill@gmail.com>
2016-07-20 16:24:28 +1000

    added debug module

---
+++ src/modules/debug.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('longjohn');
+}
+
+function high(...args){
+    console.log(...args)
+}
+function medium(...args){
+    switch(process.env.NODE_ENV){
+        case 'production':
+            break;
+        default:
+            console.log(...args);
+            break;
+    }
+}
+function low(...args){
+    switch(process.env.NODE_ENV){
+        case 'debug':
+            console.log(...args);
+            break;
+    }
+}
+export default {
+    high: high,
+    medium: medium,
+    low: low
+}
